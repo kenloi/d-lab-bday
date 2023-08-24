8a4f8097099fc74260e6eb015fa51dc27cabad33
Kenneth Loi <kennethloi@Kenneths-MacBook-Pro.local>
2023-08-23 22:32:02 -0700

    Updated animation for birthday message

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-const names = ['Alice', 'Bob', 'Charlie']; // Add the names here
-const container = document.getElementById('nameContainer');
-
-function getRandomPosition() {
-    const y = window.innerWidth;
-    const x = window.innerHeight;
-    return {
-        top: Math.floor(Math.random() * x) + 'px',
-        left: Math.floor(Math.random() * y) + 'px',
-    };
-}
-
-names.forEach((name, index) => {
-    setTimeout(() => {
-        const div = document.createElement('div');
-        div.className = 'name';
-        div.textContent = name;
-        const position = getRandomPosition();
-        div.style.top = position.top;
-        div.style.left = position.left;
-        div.style.opacity = 1;
-        container.appendChild(div);
-    }, index * 2000); // Change the delay between names here
-});
+body, html {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif;
+    background: linear-gradient(120deg, #fbc2eb 0%, #a6c1ee 100%);
+    height: 100%;
+    overflow: hidden;
+  }
+  
+  .container {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+  }
+  
+  #message {
+    font-size: 36px;
+    color: #e44d26;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    font-weight: bold;
+  }
+  
+  .name {
+    font-size: 48px;
+    color: #555;
+    position: absolute;
+    opacity: 0;
+    transition: all 1s ease;
+  }
+  
