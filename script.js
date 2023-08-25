9f13a7c9edf5b83124fea20b0b29a217bf6ef490
Kenneth Loi <kennethloi@Kenneths-MacBook-Pro.local>
2023-08-24 23:20:09 -0700

    ready to share

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 const messageContainer = document.getElementById('messageContainer');
 const month = 'August'; // Set the current month here
-const names = ['ARUSHI!!!', 'ERIN!!!!', 'RON', 'JULIA', 'MARENA', 'HANNAH']; // Add all the names here
+const names = ['ARUSHI', 'ERIN', 'RON', 'JULIA', 'MARENA', 'HANNAH']; // Add all the names here
 
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
-  async function animate() {
-    const messages = [
-      { text: `Happy ${month}`, class: 'message-month' },
-      { text: `To all the ${month} babies...`, class: 'message-babies' },
-      ...names.map((name) => ({
-        text: name,
-        class: 'message-name',
-        color: getRandomColor()
-      })),
-      { text: '[HAPPY BIRTHDAY]', class: 'message-happy-birthday' }
-    ];
-  
-    for (const { text, class: className, color } of messages) {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          const div = document.createElement('div');
-          div.className = className;
-          div.textContent = text;
-          if (color) div.style.color = color; // Apply color if defined
-          messageContainer.appendChild(div);
-          resolve();
-        }, 2000); // Change the delay between messages here
-      });
-    }
-  }
+    const hue = Math.floor(Math.random() * 360); // Hue value [0, 360]
+    const saturation = 80 + Math.floor(Math.random() * 21); // Saturation value [80, 100]
+    const lightness = 40 + Math.floor(Math.random() * 21); // Lightness value [40, 60]
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+}
+
+
+async function animate() {
+const messages = [
+    { text: `Happy ${month}!!!`, class: 'message-month' },
+    { text: `To all the ${month} babies...`, class: 'message-babies' },
+    ...names.map((name) => ({
+    text: name,
+    class: 'message-name',
+    color: getRandomColor()
+    })),
+    { text: 'HAVE A HAPPY BIRTHDAY!!!', class: 'message-happy-birthday' }
+];
+
+for (const { text, class: className, color } of messages) {
+    await new Promise((resolve) => {
+    setTimeout(() => {
+        const div = document.createElement('div');
+        div.className = className;
+        div.textContent = text;
+        if (color) div.style.color = color; // Apply color if defined
+        messageContainer.appendChild(div);
+
+        // Apply the transition/animation
+        requestAnimationFrame(() => {
+          div.style.opacity = 1;
+          div.style.transform = 'translateX(0)';
+        });
+
+        // Wait for the transition/animation to complete
+        setTimeout(resolve, 500);
+    }, 1000); // Change the delay between messages here
+    });
+}
+
+}
   
 
 animate();
